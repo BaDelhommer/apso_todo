b8a882a8df32b7324bdd178ffc966663ddd1fe7c
Brant Delhommer <bdelhommer@ascensionsheriff.com>
2024-09-18 10:53:47 -0500

    made help command work when providing a specific command as an argument

--- command_help.go
+++ command_help.go
@@ -6,13 +6,23 @@ import (
 )
 
 func commandHelp(db *database.DB, args ...string) error {
+	cmds := getCommands()
 	fmt.Println()
 	fmt.Println("Welcome to the APSO To Do app!")
 	fmt.Println("Usage:")
-	fmt.Println()
-	for _, cmd := range getCommands() {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	if len(args) == 1 {
+		if cmd, ok := cmds[args[0]]; !ok {
+			fmt.Println("unrecognized command", args[0])
+		} else if ok {
+			fmt.Println("Command: ", cmd.name)
+			fmt.Println("Description: ", cmd.description)
+		}
+	} else {
+		fmt.Println()
+		for _, cmd := range cmds {
+			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+		}
+		fmt.Println()
 	}
-	fmt.Println()
 	return nil
 }
